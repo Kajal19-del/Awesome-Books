463944d2df0ee5883cd096c353f12e3fe954ec24
johnie261 <njorogejohn261@gmail.com>
2023-02-13 21:12:46 +0300

    Add books in page and  local storage

--- main.js
+++ main.js
@@ -19,3 +19,32 @@ if (localStorage.getItem('books') !==null) {
   });
 }
 
+function addBook(bookTitle, bookAuthor) {
+  this.bookTitle = bookTitle;
+  this.bookAuthor = bookAuthor;
+}
+
+addBookBtn.addEventListener('click', (e) => {
+  e.preventDefault();
+
+  addingBooks.innerHTML += `
+  <div>
+  <h3>${item.bookTitle}</h3>
+  <p>${item.bookAuthor}</p>
+  <button class='removeBook' name=${item.bookTitle}>Remove</button>
+  <hr>
+  </div>
+  `;
+
+  const newBooks = new addBook(bookTitle.value, bookAuthor.value);
+
+  if (localStorage.getItem('books') === null) {
+    const books = [];
+    books.push(newBooks);
+    localStorage.setItem('books', JSON.stringify(books));
+  } else {
+    const books = JSON.parse(localStorage.getItem('books'));
+    books.push(newBooks);
+    localStorage.setItem('books', JSON.stringify(books));
+  }
+});
