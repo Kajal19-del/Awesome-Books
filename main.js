5b31340eb9574898b4a5cc3a108e8f40dc29bd43
Kajal Pramanik <pramanikkajal7@gmail.com>
2023-02-14 22:16:48 +0530

    Modify main.js for using JS class

--- main.js
+++ main.js
@@ -5,45 +5,31 @@ const addBookBtn = document.querySelector('#addBookBtn');
 
 let Books = [];
 
-function BookData() {
-  bookList.innerHTML = '';
-  for (let i = 0; i < Books.length; i += 1) {
-    bookList.innerHTML += `
-      <div>
-      <h3 class='booksTitle'>${Books[i].booksTitle}</h3>
-      <p class='booksAuthor'>${Books[i].booksAuthor}</p>
-      <button class='removeBook' onclick='remove(${i})'>remove</button>
-      <hr>
-      </div>
-    `;
+class AwesomeBook {
+  constructor(title, author) {
+    this.title = title;
+    this.author = author;
+  }
 
-    bookTitle.value = '';
-    bookAuthor.value = '';
+  static bookData() {
+    bookList.innerHTML = '';
+    for (let i = 0; i < Books.length; i += 1) {
+      bookList.innerHTML += `
+        <div class='library'>
+          <p class='Title'>'${Books[i].title}' by  ${Books[i].author}</p>
+          <button class='removeBook' onclick='AwesomeBook.remove(${i})'>remove</button>
+        </div>
+      `;
+
+      bookTitle.value = '';
+      bookAuthor.value = '';
+      bookTitle.focus();
+    }
+  }
+
+  static remove(index) {
+    Books.splice(index, 1);
+    AwesomeBook.bookData();
+    localStorage.setItem('Books', JSON.stringify(Books));
   }
 }
-
-// eslint-disable-next-line no-unused-vars
-function remove(index) {
-  Books.splice(index, 1);
-  BookData();
-  localStorage.setItem('Books', JSON.stringify(Books));
-}
-
-// save books in local storage and showing on page even refresh
-window.onload = () => {
-  if (localStorage.getItem('Books')) {
-    Books = JSON.parse(localStorage.getItem('Books'));
-  }
-  BookData();
-};
-
-// add books on page and local storage
-addBookBtn.addEventListener('click', () => {
-  const bookItems = {
-    booksTitle: bookTitle.value,
-    booksAuthor: bookAuthor.value,
-  };
-  Books.push(bookItems);
-  BookData();
-  localStorage.setItem('Books', JSON.stringify(Books));
-});
