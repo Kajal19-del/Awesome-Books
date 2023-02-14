3dd50828323cc6aa00da4fca46f4032b51a1fd1f
Kajal Pramanik <pramanikkajal7@gmail.com>
2023-02-14 16:46:42 +0530

    Fix linters errors

--- main.js
+++ main.js
@@ -3,12 +3,11 @@ const bookTitle = document.querySelector('#bookTitle');
 const bookAuthor = document.querySelector('#bookAuthor');
 const addBookBtn = document.querySelector('#addBookBtn');
 
-// section - hold books after refresh or closing the page
 let Books = [];
 
 function BookData() {
   bookList.innerHTML = '';
-  for (let i = 0; i < Books.length; i++) {
+  for (let i = 0; i < Books.length; i += 1) {
     bookList.innerHTML += `
       <div>
       <h3 class='booksTitle'>${Books[i].booksTitle}</h3>
@@ -23,18 +22,22 @@ function BookData() {
   }
 }
 
+// eslint-disable-next-line no-unused-vars
 function remove(index) {
   Books.splice(index, 1);
   BookData();
   localStorage.setItem('Books', JSON.stringify(Books));
-};
+}
 
+// save books in local storage and showing on page even refresh
 window.onload = () => {
   if (localStorage.getItem('Books')) {
-    Books = JSON.parse(localStorage.getItem('Books'))
+    Books = JSON.parse(localStorage.getItem('Books'));
   }
   BookData();
 };
+
+// add books on page and local storage
 addBookBtn.addEventListener('click', () => {
   const bookItems = {
     booksTitle: bookTitle.value,
@@ -43,4 +46,4 @@ addBookBtn.addEventListener('click', () => {
   Books.push(bookItems);
   BookData();
   localStorage.setItem('Books', JSON.stringify(Books));
-})
+});
