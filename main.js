869e3a607a62d8631cc6159c9653969c26cbd4be
johnie261 <njorogejohn261@gmail.com>
2023-02-15 20:46:07 +0300

    Update main.js file

--- main.js
+++ main.js
@@ -55,3 +55,19 @@ addBookBtn.addEventListener('click', () => {
   AwesomeBook.bookData();
   localStorage.setItem('Books', JSON.stringify(Books));
 });
+
+listPage.addEventListener('click', () => {
+  document.querySelector('.bookLibrary').style.display = 'block';
+  document.querySelector('.bookForm').style.display = 'none';
+  document.querySelector('.contactInfo').style.display = 'none';
+})
+addPage.addEventListener('click', () => {
+  document.querySelector('.bookLibrary').style.display = 'none';
+  document.querySelector('.bookForm').style.display = 'block';
+  document.querySelector('.contactInfo').style.display = 'none';
+})
+contactPage.addEventListener('click', () => {
+  document.querySelector('.bookLibrary').style.display = 'none';
+  document.querySelector('.bookForm').style.display = 'none';
+  document.querySelector('.contactInfo').style.display = 'block';
+})
